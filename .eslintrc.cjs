299afa1c8496b2e67860baf7fac6d77c7eea7365
Wyn <tranthang0546555@gmail.com>
2024-11-20 23:04:33 +0700

    config lib ver

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,6 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn'
+    'react-refresh/only-export-components': 'off'
   }
 }
