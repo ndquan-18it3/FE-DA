cde0288687e66bb176ac1d9731a96b2d2c64f4a2
Wyn <tranthang0546555@gmail.com>
2024-11-20 23:33:05 +0700

    recommit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,8 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
-    // 'react-refresh/only-export-components': 'off'
+    'react-refresh/only-export-components': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
   }
 }
