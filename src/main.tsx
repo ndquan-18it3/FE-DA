4833be2c98c03a8d5ff06388b42edfbe38397de3
ntnhan18it3 <ntnhan.18it3@vku.udn.vn>
2024-12-11 08:26:50 +0000

    recommit

--- src/main.tsx
+++ src/main.tsx
@@ -28,7 +28,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     />
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   </Provider>
   // </React.StrictMode>,
